cdce6e7e9b7a6ea651fdf39c0687945435bfc2f0
Paul V. Klimov <pklimov@Pauls-MacBook-Pro.local>
2017-09-30 08:38:22 -0700

    add initialize_quandl

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,5 +1,7 @@
 """General utilities"""
 
+import quandl
+
 API_KEYS_DIR = ''
 API_KEYS_FILE = 'api_keys.rtf'
 
@@ -24,7 +26,26 @@ def get_api_key(directory=API_KEYS_DIR,
     Args: 
         directory (str): directory containing api key.
         filename (str): filename containing api key. 
-        source (str): api source, e.g. QUANDL. 
+        source (str): api source, e.g. QUANDL.
+
+
     """
     text = open(directory+filename, 'r').read()
     return text.split(source + ' ')[1].split('\\\n')[0]
+
+
+def initialize_quandl(directory=API_KEYS_DIR,
+                      filename=API_KEYS_FILE,
+                      source='QUANDL'):
+    """Initialize QUANDL API.
+
+    mutates quandl.ApiConfig.api_key
+
+    Args:
+        directory (str): directory containing api key.
+        filename (str): filename containing api key.
+        source (str): api source, e.g. QUANDL.
+    """
+    quandl.ApiConfig.api_key = get_api_key(directory=directory,
+                                           filename=filename,
+                                           source=source)
